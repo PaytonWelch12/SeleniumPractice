6eba00a26c9e376d353387c5cc9f4ce5387f473b
PaytonWelch12 <patewelch1@gmail.com>
2024-03-01 11:05:06 -0600

    Chrome Driver Practice - File Upload

--- SelPractice/src/SelPackage/FirstChromeDriver.java
+++ SelPractice/src/SelPackage/FirstChromeDriver.java
@@ -1,10 +1,38 @@
 package SelPackage;
 
-public class FirstChromeDriver {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	import java.time.Duration;
+
+	import org.openqa.selenium.By;  
+	import org.openqa.selenium.WebDriver;  
+	import org.openqa.selenium.chrome.ChromeDriver;
+	import org.openqa.selenium.support.ui.ExpectedConditions;
+	import org.openqa.selenium.support.ui.WebDriverWait;  
+
+
+public class FirstChromeDriver {
+	
+		
+
+		public static void main(String[] args) {
+			
+		    // declaration and instantiation of objects/variables  
+		    System.setProperty("webdriver.chrome.driver", "D:\\ChromeDriver\\chromedriver.exe");  
+		    WebDriver driver=new ChromeDriver();  
+		      
+		    // Launch web
+		    driver.navigate().to("http://www.google.com/");  
+		          
+		    // Click on the search text box and send value  
+		    driver.findElement(By.name("q")).sendKeys("sample search");  
+		    
+		          
+		    // Click on the search button  
+		    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10)); // Adjust time as necessary
+		    wait.until(ExpectedConditions.elementToBeClickable(By.name("btnK"))).click();
+		 
+		       
+		    driver.quit();  
+		}
 
 	}
-
-}
