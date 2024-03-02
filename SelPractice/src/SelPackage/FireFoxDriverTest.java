e87785a1c352321fe04391770fcbeb15c83b6d04
PaytonWelch12 <patewelch1@gmail.com>
2024-03-02 13:56:39 -0600

    FireFoxDriver - Update

--- SelPractice/src/SelPackage/FireFoxDriverTest.java
+++ SelPractice/src/SelPackage/FireFoxDriverTest.java
@@ -1,34 +1,31 @@
 package SelPackage;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
-	import org.openqa.selenium.By;
-	import org.openqa.selenium.JavascriptExecutor;
-	import org.openqa.selenium.WebDriver;
-	import org.openqa.selenium.WebElement;
-	import org.openqa.selenium.firefox.FirefoxDriver;
-	import org.openqa.selenium.firefox.FirefoxOptions;
-	 
-	  
-	public class FireFoxDriverTest {  
-	  
-	    public static void main(String[] args) {  
-	          
-	   
-	    System.setProperty("webdriver.gecko.driver","D:\\GeckoDriver\\geckodriver.exe" );  
-	          
-	    FirefoxOptions options = new FirefoxOptions();
-	    options.setBinary("C:/Program Files/Mozilla Firefox/firefox.exe");
-	 
-	    WebDriver driver = new FirefoxDriver(options);
-	          
-	    driver.navigate().to("https://paytonwelch12.github.io/WebsitePW/");  
-	     
-	    WebElement element = driver.findElement(By.id("portfolio"));
-	     
-	    ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
-	     
-	    driver.quit();  
-	      
-	    }  
-	  
-	}  
+public class FireFoxDriverTest {
+
+	public static void main(String[] args) {
+
+		System.setProperty("webdriver.gecko.driver", "D:\\GeckoDriver\\geckodriver.exe");
+
+		FirefoxOptions options = new FirefoxOptions();
+		options.setBinary("C:/Program Files/Mozilla Firefox/firefox.exe");
+
+		WebDriver driver = new FirefoxDriver(options);
+
+		driver.navigate().to("https://paytonwelch12.github.io/WebsitePW/");
+
+		WebElement element = driver.findElement(By.id("portfolio"));
+
+		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
+
+		driver.quit();
+
+	}
+
+}
