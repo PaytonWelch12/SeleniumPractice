f8c5321c8c16504f053ae67fb9579153f7991e29
PaytonWelch12 <patewelch1@gmail.com>
2024-03-02 13:56:22 -0600

    Microsoft Edge Driver - Update

--- SelPractice/src/SelPackage/MicrosoftEdgeDriverTest.java
+++ SelPractice/src/SelPackage/MicrosoftEdgeDriverTest.java
@@ -10,43 +10,42 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;  
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class MicrosoftEdgeDriverTest {
 
-	public static void main(String[] args) {	  
-		
-        System.setProperty("webdriver.edge.driver", "D:\\Edge Driver\\msedgedriver.exe");
-            
-        EdgeOptions options = new EdgeOptions();
-    
-        WebDriver driver = new EdgeDriver(options);
-             
-        driver.get("http://www.google.com");
-        
-        driver.findElement(By.name("q")).sendKeys( "google news");
-		 
-		 WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10)); 
-		 
-		 wait.until(ExpectedConditions.elementToBeClickable(By.name("btnK"))).click();
-		 
-	     wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("search")));
+	public static void main(String[] args) {
 
-	     try {
-	         wait.until(ExpectedConditions.elementToBeClickable(By.partialLinkText("Google News"))).click();
-	     } catch (Exception e) {
-	         System.out.println("Failed to find Google News link in search results.");	          
-	     }
-		 
-		 JavascriptExecutor js = (JavascriptExecutor)driver;
-		 
-		 try {
-		 js.executeScript("scrollBy(0,5000)");
-		 }
-		 catch (Exception f){
-			 System.out.println("Failed to scroll down to look at news.");
-		 }
-		 
-		 driver.quit();  
-    }
+		System.setProperty("webdriver.edge.driver", "D:\\Edge Driver\\msedgedriver.exe");
+
+		EdgeOptions options = new EdgeOptions();
+
+		WebDriver driver = new EdgeDriver(options);
+
+		driver.get("http://www.google.com");
+
+		driver.findElement(By.name("q")).sendKeys("google news");
+
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+		wait.until(ExpectedConditions.elementToBeClickable(By.name("btnK"))).click();
+
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("search")));
+
+		try {
+			wait.until(ExpectedConditions.elementToBeClickable(By.partialLinkText("Google News"))).click();
+		} catch (Exception e) {
+			System.out.println("Failed to find Google News link in search results.");
+		}
+
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+
+		try {
+			js.executeScript("scrollBy(0,5000)");
+		} catch (Exception f) {
+			System.out.println("Failed to scroll down to look at news.");
+		}
+
+		driver.quit();
+	}
 }
