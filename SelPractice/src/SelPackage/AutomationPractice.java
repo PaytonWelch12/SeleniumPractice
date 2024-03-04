001327d78cfb6ca55636eac0d8783d1b8e88c780
PaytonWelch12 <patewelch1@gmail.com>
2024-03-04 11:43:59 -0600

    Automation Practice

--- SelPractice/src/SelPackage/AutomationPractice.java
+++ SelPractice/src/SelPackage/AutomationPractice.java
@@ -1,27 +1,98 @@
 package SelPackage;
 
+import java.time.Duration;
+
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.UnhandledAlertException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import static org.junit.jupiter.api.Assertions.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AutomationPractice {
-	
-	public static void main(String[] args) {
 
-	        System.setProperty("webdriver.chrome.driver", "D:\\ChromeDriver\\chromedriver.exe");
+//Tests to see if a sites authentication functionality
+	public static String authTest() {
 
-	        ChromeOptions options = new ChromeOptions();
-	        options.addArguments("--start-maximized");
-	        WebDriver driver = new ChromeDriver(options);
+		// Web Driver Path
+		System.setProperty("webdriver.chrome.driver", "D:\\ChromeDriver\\chromedriver.exe");
 
-	        driver.navigate().to("https://the-internet.herokuapp.com/");
-	        driver.findElement(By.partialLinkText("A/B")).click();
-	        
-	        
-	        String myString = driver.findElement(By.cssSelector("p")).getText();	        
-	        
-	        driver.quit();
+		// Objects Initiated and Max Screen Arg
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--start-maximized");
+		WebDriver driver = new ChromeDriver(options);
+
+		// Sample Web Site Load with Embedded Credentials for Basic Auth
+		String basicAuthUrl = "https://admin:admin@the-internet.herokuapp.com/basic_auth";
+
+		// Navigate to the page with Basic Auth
+		driver.navigate().to(basicAuthUrl);
+
+		// Get Text For Confirmed Auth
+		String myString = driver.findElement(By.cssSelector("p")).getText();
+
+		//Test Logic
+		if (myString.equals("Congratulations! You must have the proper credentials.")) {
+			myString = "Passed";
+		} else {
+			myString = "Failed";
 		}
+
+		// Quit and Exit
+		driver.quit();
+		return myString;
+	}
+
+//Tests to see if content is available despite its dynamic nature
+	public static String contextMenuTest() {
+		//Return String
+		String myString;
+		// Web Driver Path
+			System.setProperty("webdriver.chrome.driver", "D:\\ChromeDriver\\chromedriver.exe");
+
+		// Objects Initiated and Max Screen Arg
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("--start-maximized");
+			WebDriver driver = new ChromeDriver(options);
+			
+		//
+			driver.navigate().to("https://the-internet.herokuapp.com/context_menu");
+		
+		//Find and Store Element
+			WebElement element = driver.findElement(By.id("hot-spot"));
+			
+		//Action Object
+			Actions action = new Actions(driver);
+			
+
+		//Right Click
+			action.contextClick(element).perform();
+			
+		//See if the Alert is Present
+		try {
+			 //Wait for the alert
+			  Alert alert = new WebDriverWait(driver, Duration.ofSeconds(5))
+			                        .until(ExpectedConditions.alertIsPresent());
+			  
+			 //Test Logic
+			  myString = "Passed";
+			  
+			 //Accepting the Alert
+			  alert.accept();
+			    
+			} catch(NoAlertPresentException e){
+				
+				myString = "Failed";
+			}finally {
+				driver.quit();
+			}
+				
+
+		return myString;
+	}
 }
---
+++ SelPractice/src/SelPackage/DriverProgram.java
@@ -0,0 +1,25 @@
+package SelPackage;
+
+public class DriverProgram {
+	public static void main(String[] args) {
+		
+		
+		//Text
+		System.out.println("___________Selenium Tests___________");
+		
+		//Auth Test Google
+		System.out.println("Authentication Test: " + AutomationPractice.authTest());
+		
+		//
+		System.out.println("Context Menu Test: " + AutomationPractice.contextMenuTest());
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
