572bbde4cd970848d80793df2d7594f294a751ce
PaytonWelch12 <patewelch1@gmail.com>
2024-03-04 12:21:23 -0600

    Context Menu and Content Test Addition

--- SelPractice/src/SelPackage/AutomationPractice.java
+++ SelPractice/src/SelPackage/AutomationPractice.java
@@ -5,7 +5,7 @@ import java.time.Duration;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.UnhandledAlertException;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -15,6 +15,37 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AutomationPractice {
+
+//Tests to see if content is present on a site
+	public static String contentTest() {
+		String myString;
+		// Web Driver Path
+		System.setProperty("webdriver.chrome.driver", "D:\\ChromeDriver\\chromedriver.exe");
+
+		// Objects Initiated and Max Screen Arg
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--start-maximized");
+		WebDriver driver = new ChromeDriver(options);
+
+		driver.navigate().to("https://the-internet.herokuapp.com");
+
+		try {		
+			// Wait for item avalibilty
+			WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+			wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[contains(text(), 'Welcome')]")));
+
+			myString = "Passed";
+
+		} catch (TimeoutException e) {
+			myString = "Failed";
+			
+		} finally {
+			driver.quit();
+			
+		}
+
+		return myString;
+	}
 
 //Tests to see if a sites authentication functionality
 	public static String authTest() {
@@ -36,7 +67,7 @@ public class AutomationPractice {
 		// Get Text For Confirmed Auth
 		String myString = driver.findElement(By.cssSelector("p")).getText();
 
-		//Test Logic
+		// Test Logic
 		if (myString.equals("Congratulations! You must have the proper credentials.")) {
 			myString = "Passed";
 		} else {
@@ -48,50 +79,43 @@ public class AutomationPractice {
 		return myString;
 	}
 
-//Tests to see if content is available despite its dynamic nature
+//Tests to see if context Menu is Present
 	public static String contextMenuTest() {
-		//Return String
+		// Return String
 		String myString;
+
 		// Web Driver Path
-			System.setProperty("webdriver.chrome.driver", "D:\\ChromeDriver\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "D:\\ChromeDriver\\chromedriver.exe");
 
 		// Objects Initiated and Max Screen Arg
-			ChromeOptions options = new ChromeOptions();
-			options.addArguments("--start-maximized");
-			WebDriver driver = new ChromeDriver(options);
-			
-		//
-			driver.navigate().to("https://the-internet.herokuapp.com/context_menu");
-		
-		//Find and Store Element
-			WebElement element = driver.findElement(By.id("hot-spot"));
-			
-		//Action Object
-			Actions action = new Actions(driver);
-			
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--start-maximized");
+		WebDriver driver = new ChromeDriver(options);
 
-		//Right Click
-			action.contextClick(element).perform();
-			
-		//See if the Alert is Present
+		driver.navigate().to("https://the-internet.herokuapp.com/context_menu");
+
+		// Find and Store Element
+		WebElement element = driver.findElement(By.id("hot-spot"));
+
+		// Action Object
+		Actions action = new Actions(driver);
+
+		// Right Click
+		action.contextClick(element).perform();
+
+		// Is the Alert Present
 		try {
-			 //Wait for the alert
-			  Alert alert = new WebDriverWait(driver, Duration.ofSeconds(5))
-			                        .until(ExpectedConditions.alertIsPresent());
-			  
-			 //Test Logic
-			  myString = "Passed";
-			  
-			 //Accepting the Alert
-			  alert.accept();
-			    
-			} catch(NoAlertPresentException e){
-				
-				myString = "Failed";
-			}finally {
-				driver.quit();
-			}
-				
+			Alert alert = new WebDriverWait(driver, Duration.ofSeconds(5)).until(ExpectedConditions.alertIsPresent());
+			myString = "Passed";
+			alert.accept();
+
+		} catch (NoAlertPresentException e) {
+			myString = "Failed";
+
+		} finally {
+			driver.quit();
+
+		}
 
 		return myString;
 	}
--- SelPractice/src/SelPackage/DriverProgram.java
+++ SelPractice/src/SelPackage/DriverProgram.java
@@ -7,10 +7,13 @@ public class DriverProgram {
 		//Text
 		System.out.println("___________Selenium Tests___________");
 		
+		//Content Presence test
+		System.out.println("Content Test: " + AutomationPractice.contentTest());
+		
 		//Auth Test Google
 		System.out.println("Authentication Test: " + AutomationPractice.authTest());
 		
-		//
+		//Context menu Test
 		System.out.println("Context Menu Test: " + AutomationPractice.contextMenuTest());
 		
 		
